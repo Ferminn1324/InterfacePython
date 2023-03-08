ed870c541f49bdcdb750fb53ddae627c020e4305
Fermin Tovar <fermin_tovar1@hotmail.com>
2023-03-07 18:01:31 -0600

    actualizacion

--- Conversor.py
+++ Conversor.py
@@ -3,6 +3,7 @@
 from tkinter import *
 # * significa con todos los elementos 
 from tkinter import ttk
+from tkinter import messagebox
 
 class Conversor:
     def __init__(self,raiz):
@@ -11,17 +12,19 @@ class Conversor:
         self.pies = StringVar()
         self.metros = StringVar()
 
-        mainFrame = ttk.Frame(raiz)
+        mainFrame = ttk.Frame(raiz, padding="10 10 30 30")#Padding(Izquierda,Arriba, Derecha, Abajo)
         mainFrame.grid(column=1, row=0)
         
-        PiesEntry = ttk.Entry(mainFrame, textvariable=self.pies)
-        PiesEntry.grid(column=1, row=0)
+        self.PiesEntry = ttk.Entry(mainFrame, width=12, textvariable=self.pies)
+        self.PiesEntry.grid(column=1, row=0,sticky=(W,E))
+
         ttk.Label(mainFrame, text= "pies").grid(column=2,row=0)
         ttk.Label(mainFrame,text ="Son equivalentes a").grid(column=0,row=1)
         ttk.Label(mainFrame,textvariable=self.metros).grid(column=1,row=1)
         ttk.Label(mainFrame,text ="Metros").grid(column=2,row=1)
 
         ttk.Button(mainFrame,text= "Convertir",command=self.Calcular).grid(column=2,row=2)
+        self.PiesEntry.focus()
         #Hacer la operacion haciendo enter
         raiz.bind("<Return>",self.Calcular)
 
@@ -30,7 +33,14 @@ class Conversor:
         print("Boton Presionado") 
         piesUsuario =self.pies.get() #Siempre devuelve cadena.
         print("Pies Ingresados",piesUsuario)
-        piesFlotante = float(piesUsuario)#Conversion de cadena a flotante
-        metros = piesFlotante * 0.3048
-        print("Metros:", metros)
-        self.metros.set(metros)
+        try:
+           piesFlotante = float(piesUsuario)#Conversion de cadena a flotante
+           metros = piesFlotante * 0.3048
+           print("Metros:", metros)
+           self.metros.set(metros)
+        except:
+            messagebox.showinfo("Error")
+            print("No es un dato valido.")
+            self.pies.set("")
+
+
---
+++ Sesion.py
@@ -0,0 +1,32 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+
+
+class Sesion:
+    def __init__(self,raiz):
+        raiz.title("Inicio de sesion")
+
+
+        self.usuario = StringVar()
+        self.Contraseña = StringVar()
+
+        mainFrame = ttk.Frame(raiz,padding="10 10 12 12")
+        mainFrame.grid(column=1, row=0)
+
+        self.usuarioEntry = ttk.Entry(mainFrame, width=25, textvariable=self.usuario)
+        self.usuarioEntry.grid(column=1,row=0)
+        
+        self.ContraseñaEntry = ttk.Entry(mainFrame,width=25,textvariable=self.Contraseña)
+        self.ContraseñaEntry.grid(column=1,row=2)
+
+
+        ttk.Label(mainFrame, text= "Usuario:").grid(column=0,row=0)
+        ttk.Label(mainFrame,text="").grid(column=0,row=1)
+        ttk.Label(mainFrame,text="").grid(column=0,row=3)
+        ttk.Label(mainFrame,text ="Contraseña:").grid(column=0,row=2)
+        ttk.Label(mainFrame,text="").grid(column=0,row=3)
+        
+        ttk.Button(mainFrame,text= "Ingresar").grid(column=1,row=4,sticky=(E,S))
+
